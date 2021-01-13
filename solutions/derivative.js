10b52187425884cd78b4de85dcd0ef7e641ad4ff
rjsiinghhh <rjsiinghhh@gmail.com>
2021-01-13 15:39:14 -0800

    13

---
+++ solutions/derivative.js
@@ -0,0 +1,26 @@
+// take two number as the parameters 
+
+// first number being the coeffiencent 
+
+// second being the exponent
+
+
+//function should multiply the two numbers and then subtract 1 from the exponent
+
+// has to print out an expression like (28x ^7) 
+
+
+// example (derive 7, 8) 
+
+
+function derive(a,b) {
+return ( a* b) + "x^" + (b -1);
+    // expression:  '(multiplied numbers) x ^(exponent - 1)'
+
+    // multiply cooefficient * exponent
+
+    // subtract exponent - 1
+
+    // output the expression
+
+  }
---
+++ solutions/excel.js
@@ -0,0 +1,22 @@
+const { assertPlaceholder } = require("@babel/types");
+
+class Solution {
+    public:
+    string convertToTitle(int n) {
+
+
+
+        const alpha = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+
+
+        for(let i = 0; i< alpha.length; i++){
+
+        }        
+        console.log(alpha);
+       
+       
+        // give each number a letter in the alphabet(a -1 , b-2, c-3)
+        // once you hit 27, 
+
+    }
+};
